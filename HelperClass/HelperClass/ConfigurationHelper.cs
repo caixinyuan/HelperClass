e0104e72d30c2d4c2c5fc7e21edc8fa0c5d39f5a
318900260@qq.com <Cxycxy123>
2019-08-19 23:28:34 +0800

    操作类

---
+++ HelperClass/HelperClass/ConfigurationHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace HelperClass
+{
+   public static class ConfigurationHelper
+    {
+        /// <summary>
+        /// 获取AppSetting的配置
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static T GetAppSettings<T>(string key) where T : class, new()
+        {
+            var baseDir = AppContext.BaseDirectory;
+            var currentClassDir = Directory.GetCurrentDirectory();//获取根目录
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(currentClassDir)
+                .Add(new JsonConfigurationSource { Path = "appsettings.json", Optional = false, ReloadOnChange = true })
+                .Build();
+            var appconfig = new ServiceCollection()
+                .AddOptions()
+                .Configure<T>(config.GetSection(key))    ///有问题
+                .BuildServiceProvider()
+                .GetService<IOptions<T>>()
+                .Value;
+            return appconfig;
+        }
+
+        private static void Configure<T>() where T : class, new()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HelperClass/HelperClass/JsonHelper.cs
@@ -0,0 +1,85 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace HelperClass
+{
+    /// <summary>
+    /// 
+    /// 来源于开源项目LdCms
+    /// 
+    /// 
+    /// Json操作类
+    /// </summary>
+   public static class JsonHelper
+    {
+        public static bool IsJson(this string str)
+        {
+            try
+            {
+                Newtonsoft.Json.Linq.JObject.Parse(str);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public static object ToJson(this string json)
+        {
+            return json == null ? null : Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+        }
+        public static string ToJson(this object obj)
+        {
+            var timeConverter = new Newtonsoft.Json.Converters.IsoDateTimeConverter { DateTimeFormat = "yyyy-MM-dd HH:mm:ss" };
+            return Newtonsoft.Json.JsonConvert.SerializeObject(obj, timeConverter);
+        }
+        public static string ToJson(this object obj, string datetimeformats)
+        {
+            var timeConverter = new Newtonsoft.Json.Converters.IsoDateTimeConverter { DateTimeFormat = datetimeformats };
+            return Newtonsoft.Json.JsonConvert.SerializeObject(obj, timeConverter);
+        }
+        public static T ToObject<T>(this string json)
+        {
+            return json == null ? default(T) : Newtonsoft.Json.JsonConvert.DeserializeObject<T>(json);
+        }
+        public static T ToObject<T>(this object obj)
+        {
+            string json = obj.ToJson();
+            return json == null ? default(T) : Newtonsoft.Json.JsonConvert.DeserializeObject<T>(json);
+        }
+
+        /// <summary>
+        /// 转List集合，反序列化
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="json"></param>
+        /// <returns></returns>
+        public static List<T> ToList<T>(this string json)
+        {
+            return json == null ? null : Newtonsoft.Json.JsonConvert.DeserializeObject<List<T>>(json);
+        }
+
+        /// <summary>
+        /// json转DataTable
+        /// </summary>
+        /// <param name="json"></param>
+        /// <returns></returns>
+        public static DataTable ToTable(this string json)
+        {
+            return json == null ? null : Newtonsoft.Json.JsonConvert.DeserializeObject<DataTable>(json);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="json"></param>
+        /// <returns></returns>
+        public static JObject ToJObject(this string json)
+        {
+            return json == null ? Newtonsoft.Json.Linq.JObject.Parse("{}") : Newtonsoft.Json.Linq.JObject.Parse(json.Replace("&nbsp;", ""));
+        }
+    }
+}
